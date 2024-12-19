4667df8dc3e16a688e2f57154b65d1a8da20a6ad
guilhermebarbosac-dev <guilhermebarbosacdev@gmail.com>
2024-12-19 15:25:31 -0300

    fix page stack

--- app/_components/Stacks.tsx
+++ app/_components/Stacks.tsx
@@ -23,7 +23,7 @@ export default function Stacks() {
           {siteConfig.stacks.title}
         </h1>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 justify-items-center">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center rounded-lg shadow-md">
           {siteConfig.stacks.list.map((stack) => (
             <motion.div
               key={stack}
